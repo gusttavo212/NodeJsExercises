75dd7c15d413f387783eb79a8f203d90c85f1108
gusttavo212 <gusttavo212@gmail.com>
2018-12-02 19:24:37 -0200

    Module 1 end commit

--- Modulo01/03-nodejs Events/index.js
+++ Modulo01/03-nodejs Events/index.js
@@ -8,7 +8,7 @@ const nomeEvento = 'usuario:Click'
 meuEmissor.on(nomeEvento, function(click){
     console.log('Um usuario clicou: ',click)
 })
-
+/*
 meuEmissor.emit(nomeEvento, 'barra de rolagem')
 meuEmissor.emit(nomeEvento, 'imagem 1')
 
@@ -17,3 +17,9 @@ let count = 0
 setInterval(function() {
     meuEmissor.emit(nomeEvento, 'no ok' + (count++))
 }, 1000)
+*/
+
+const stdin = process.openStdin()
+stdin.addListener('data',function (value){
+    console.log(`Voce digitou: ${value.toString().trim()}`)
+})
